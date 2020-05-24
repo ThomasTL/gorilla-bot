75cea9df72feebca197663cee9678c4e733c4c3f
Thomas <thomas.tarpin@gmail.com>
2020-05-24 18:20:51 +0800

    Changing one folder name

--- src/Trader/index.js
+++
@@ -1,7 +0,0 @@
-const Trader = require('./trader');
-const PaperTrading = require('./paper-trading');
-const ZignalyTrading = require('./zignaly-trading');
-
-exports.Trader = Trader
-exports.PaperTrading = PaperTrading
-exports.ZignalyTrading = ZignalyTrading
